d830fd927219192aa23785e466d8428a9e358926
RAY <ray1234567890rtz@gmail.com>
2025-02-02 19:47:04 -0600

    i add something to html

---
+++ test.js
@@ -0,0 +1,14 @@
+// write a fibancci function that returns the nth number in the sequence
+// write a test for the function
+// write a test for the function that tests the first 10 numbers in the sequence
+// write a test for the function that tests the first 20 numbers in the sequence
+// write a test for the function that tests the first 30 numbers in the sequence
+// write a test for the function that tests the first 40 numbers in the sequence
+// write a test for the function that tests the first 50 numbers in the sequence
+// write a test for the function that tests the first 60 numbers in the sequence
+// write a test for the function that tests the first 70 numbers in the sequence
+// write a test for the function that tests the first 80 numbers in the sequence
+// write a test for the function that tests the first 90 numbers in the sequence
+// write a test for the function that tests the first 100 numbers in the sequence
+// write a test for the function that tests the first 110 numbers in the sequence
+// write a test for the function that tests the first 120 numbers in the sequence
